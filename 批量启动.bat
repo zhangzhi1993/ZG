9ec8a79c46bcc00fcd2df5a62da54afd4c8b6cd0
zhangzhiya1993 <zhangzhiya1993@gmail.com>
2019-10-22 10:25:51 +0800

    批处理改为相对路径

--- 批量启动.bat
+++ 批量启动.bat
@@ -1,7 +1,7 @@
-start E:\Z\Release\Construction\Bin\ZMDProxy.exe
+start .\Release\Construction\Bin\ZMDProxy.exe
 
-start E:\Z\Release\Construction\Bin\ZMDWorker.exe
-start E:\Z\Release\Construction\Bin\ZMDWorker.exe
-start E:\Z\Release\Construction\Bin\ZMDWorker.exe
+start .\Release\Construction\Bin\ZMDWorker.exe
+start .\Release\Construction\Bin\ZMDWorker.exe
+start .\Release\Construction\Bin\ZMDWorker.exe
 
-start E:\Z\Release\Construction\Bin\ZMDClient.exe 20 5
+start .\Release\Construction\Bin\ZMDClient.exe 20 5
