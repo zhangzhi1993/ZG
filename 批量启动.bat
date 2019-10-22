22270b42dabfcdccc6cb320023cb20d60255f788
zhangzhiya1993 <zhangzhiya1993@gmail.com>
2019-10-22 10:05:24 +0800

    批处理

---
+++ 批量启动.bat
@@ -0,0 +1,7 @@
+start E:\Z\Release\Construction\Bin\ZMDProxy.exe
+
+start E:\Z\Release\Construction\Bin\ZMDWorker.exe
+start E:\Z\Release\Construction\Bin\ZMDWorker.exe
+start E:\Z\Release\Construction\Bin\ZMDWorker.exe
+
+start E:\Z\Release\Construction\Bin\ZMDClient.exe 20 5
